3a7ade8fb7d405a2966c10bb9c1da5b1f2a027d8
Andrew Boxall <42051071+AndrewBoxall@users.noreply.github.com>
2019-03-25 22:31:33 +1300

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import Header from './Components/Header/Header';
 import HomepageBody from './Components/Body/HomeBody';
 import Footer from './Components/Footer/Footer';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fab, fas);
 
 class App extends Component {
   render() {
