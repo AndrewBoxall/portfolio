6e811173120c1620132ce98074c5750023d2a3cf
Andrew Boxall <42051071+AndrewBoxall@users.noreply.github.com>
2019-03-28 10:28:27 +1300

    Add files via upload

--- frontend/src/Components/Body/Skills/SkillsSection.js
+++ frontend/src/Components/Body/Skills/SkillsSection.js
@@ -10,20 +10,20 @@ class SkillsSection extends Component {
         <div>
           <h2>Technical Skills</h2>
           <p>I am always looking to learn the latest technologies and expand my skillset</p>
-          <div className="container-row">
+          <div className="skills-container-row">
             <div><FontAwesomeIcon icon={['fab', 'html5']} /> HTML 5</div>
             <div><FontAwesomeIcon icon={['fab', 'css3-alt']} /> CSS 3</div>
             <div><FontAwesomeIcon icon={['fab', 'js']} /> Javascript</div>
             <div><FontAwesomeIcon icon={['fab', 'less']} /> LESS</div>
           </div>
-          <div className="container-row">
+          <div className="skills-container-row">
             <div><FontAwesomeIcon icon={['fab', 'react']} /> React</div>
           </div>
-          <div className="container-row">
+          <div className="skills-container-row">
             <div><FontAwesomeIcon icon={['fab', 'node-js']} /> Node JS</div>
             <div><FontAwesomeIcon icon={['fab', 'github']} /> Github</div>
           </div>
-          <div className="container-row">
+          <div className="skills-container-row">
             <div><FontAwesomeIcon icon={['fas', 'database']}  /> MS SQL / Oracle / Mongo</div>
           </div>
         </div>
