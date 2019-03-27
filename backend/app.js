43e56a40de10ad4d6f9c957a23149b07093a6c88
Andrew Boxall <42051071+AndrewBoxall@users.noreply.github.com>
2019-03-27 20:56:31 +1300

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
+app.use(express.static('public/staticFiles'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
