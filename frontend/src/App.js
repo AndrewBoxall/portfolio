e2309af87e97d83b6090f25cc1628038d58a7f37
Andrew Boxall <42051071+AndrewBoxall@users.noreply.github.com>
2019-04-23 20:58:02 +1200

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import './App.css';
-
-import Header from './Components/Header/Header';
-import ProjectsSection from './Components/Body/Projects/ProjectsSection';
-import SkillsSection from './Components/Body/Skills/SkillsSection';
-import TestimonialsSection from './Components/Body/Testimonials/TestimonialsSection';
-import ContactSection from './Components/Body/Contact/ContactSection';
-import Footer from './Components/Footer/Footer';
-
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+
+import Header from './Components/Header/Header';
+
+const SkillsSection = lazy(() => import('./Components/Body/Skills/SkillsSection'));
+const TestimonialsSection = lazy(() => import('./Components/Body/Testimonials/TestimonialsSection'));
+const ContactSection = lazy(() => import('./Components/Body/Contact/ContactSection'));
+const Footer = lazy(() => import('./Components/Footer/Footer'));
+const ProjectsSection = lazy(() => import('./Components/Body/Projects/ProjectsSection'));
+
 library.add(fab, fas);
 
 class App extends Component {
@@ -18,11 +19,13 @@ class App extends Component {
     return (
         <>
           <Header />
-          <ProjectsSection />
-          <SkillsSection />
-          <TestimonialsSection />
-          <ContactSection />
-          <Footer />
+          <Suspense fallback={<div></div>}>
+            <ProjectsSection />
+            <SkillsSection />
+            <TestimonialsSection />
+            <ContactSection />
+            <Footer />
+          </Suspense>
         </>
     );
   }
