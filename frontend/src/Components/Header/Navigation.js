0fb3e851494725f646d05c5eb435d0de73833d30
Andrew Boxall <42051071+AndrewBoxall@users.noreply.github.com>
2019-07-08 15:38:59 +1200

    Add files via upload

--- frontend/src/Components/Header/Navigation.js
+++ frontend/src/Components/Header/Navigation.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './Navigation.css';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import scrollToBookmark from '../../javascripts/scrollToBookmark.js';
 import logo from '../../images/aboxit-logo.png';
@@ -10,6 +9,9 @@ class Navigation extends Component {
     super(props);
 
     this.scrollToBookmark = scrollToBookmark.bind(this);
+  }
+  componentDidMount(){
+    console.log('%PUBLIC_URL%: ');
   }
   render() {
     return (
@@ -21,9 +23,8 @@ class Navigation extends Component {
             <p>MENU</p>
           </div>
           <ul className="bookmark-links-menu">
-            <li><a className="nav-link" href="projects-section" onClick={this.scrollToBookmark.bind(this, "projects-section")}>Projects</a></li>
+            <li><a className="nav-link" href="#projects-section" onClick={this.scrollToBookmark.bind(this, "projects-section")}>Projects</a></li>
             <li><a className="nav-link" href="#skills-section" onClick={this.scrollToBookmark.bind(this, "skills-section")}>Skills</a></li>
-            <li><a className="nav-link" href="#testimonials-section" onClick={this.scrollToBookmark.bind(this, "testimonials-section")}>Testimonials</a></li>
             <li><a className="nav-link" href="#contact-section" onClick={this.scrollToBookmark.bind(this, "contact-section")}>Contact</a></li>
           </ul>
         </div>
@@ -31,7 +32,7 @@ class Navigation extends Component {
         <ul className="helper-links-menu">
           <li><a href="https://github.com/AndrewBoxall" target="_blank" rel="noopener noreferrer">GitHub</a></li>
           <li>
-            <a id="download-link" href="http://andrewjamesboxall.co.nz:3000/Andrew Boxall CV.pdf" target="_blank" rel="noopener noreferrer">CV
+            <a id="download-link" href="Andrew_Boxall_CV.pdf" target="_blank" rel="noopener noreferrer">CV
               <FontAwesomeIcon className="fa-download-icon" icon={['fas', 'file-download']}  />
             </a>
           </li>
