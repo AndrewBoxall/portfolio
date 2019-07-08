e54c98a9e16e9629bae7450a4adab519541a9bc4
Andrew Boxall <42051071+AndrewBoxall@users.noreply.github.com>
2019-07-08 15:38:32 +1200

    Add files via upload

--- frontend/src/javascripts/scrollToBookmark.js
+++ frontend/src/javascripts/scrollToBookmark.js
@@ -1,6 +1,5 @@
 export default function scrollToBookmark(linkId, e){
   e.preventDefault();
 
-  let bookmarkedElement = document.getElementById(linkId);
-  bookmarkedElement.scrollIntoView();
+  document.getElementById(linkId).scrollIntoView();
 }
